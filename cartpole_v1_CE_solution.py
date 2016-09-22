8a3a94f06c28a5454757c6e6cfc3a9cb7e0f82ce
wlw <liweiwuhome@hotmail.com>
2016-09-22 21:59:49 +0800

    finish the problem cartpole-v1

--- cartpole_v1_CE_solution.py
+++ cartpole_v1_CE_solution.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     input_space = 4
     action_space = 2
     env = gym.make('CartPole-v1')
-    env.monitor.start('/tmp/cartpole-experiment-2')
+    env.monitor.start('/tmp/cartpole-experiment-3')
     robot = GaussLinearCERobot(action_space, input_space,
                                np.zeros(action_space*input_space), np.eye(input_space*action_space),
-                               noise_begin=5, noise_delta=0.01, try_number=100, ru=0.2)
+                               noise_begin=5, noise_delta=0.01, try_number=50, ru=0.2)
     simulator = Simulator(env, robot, verbose=True, save_path='CartPole-v0/DQN/2.jpg')
-    simulator.run(episode_number=1000, max_episode_length=500, has_loss=False)
+    simulator.run(episode_number=1000, max_episode_length=1000, has_loss=False)
     env.monitor.close()
