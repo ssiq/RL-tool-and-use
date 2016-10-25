1eec471c0fcf5256f11e38e4e3ae5f5a9544edc4
wlw <liweiwuhome@hotmail.com>
2016-10-25 21:51:05 +0800

    add part ddpg, not complete

--- rl_tool/PGRobot.py
+++ rl_tool/PGRobot.py
@@ -1,5 +1,9 @@
 from rl_tool.robot import Robot
 import numpy as np
+import lasagne
+import theano
+import theano.tensor as T
+import copy
 
 
 class PGRobot(Robot):
@@ -111,3 +115,88 @@ class ContinuousMonteCarloPGRobot(MonteCarloPGRobot):
         return multivariate_normal(action_probability[:self.output_space],
                                    action_probability[self.output_space:].reshape(self.output_space,
                                                                                   self.output_space))
+
+
+class DDPG(PGRobot):
+    def __init__(self, input_shape, output_shape, random_process, memory, batch_size=32, gamma=0.99, tao=0.001, ):
+        super(DDPG, self).__init__()
+        self.random_process = random_process
+        self.memory = memory
+        self.old_state = None
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.tao = tao
+        self.state_input_var, self.action_input_var = T.matrices('policy_input', 'Qvalue_input')
+        self.policy_network, self.Qvalue_network = self._build_network(input_shape, output_shape, self.input_var)
+        self.target_policy_network = copy.deepcopy(self.policy_network)
+        self.target_Qvalue_network = copy.deepcopy(self.Qvalue_network)
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        build_predict = lambda x: [(self._build_predict_function(p, self.state_input_var),
+                                   self._build_predict_function(q, self.action_input_var)) for (p, q) in x]
+        (self.policy_predict_function, self.Qvalue_predict_function), \
+        (self.target_policy_predict_function, self.target_Qvalue_predict_function) = \
+            build_predict([(self.policy_network, self.Qvalue_network), (self.target_policy_network, self.target_Qvalue_network)])
+
+    def _build_grad_function(self, state_input_var, action_input_var):
+        actions, next_states = T.matrices('actions', 'next_states')
+        dones = T.bmatrix('dones')
+
+
+    def _update_target_network(self, tao):
+        original = [self.policy_network, self.Qvalue_network]
+        target = [self.target_policy_network, self.target_Qvalue_network]
+        for o, t in zip(original, target):
+            t_values = lasagne.layers.get_all_param_values(t)
+            o_values = lasagne.layers.get_all_param_values(o)
+            new_t_values = [tao*ov + (1-tao)*tv for tv, ov in zip(t_values, o_values)]
+            lasagne.layers.set_all_param_values(t, new_t_values)
+
+    def _build_predict_function(self, network, input_var):
+        prediction = lasagne.layers.get_output(network, allow_input_downcast=True)
+        return theano.function(input_var, prediction)
+
+    def _build_network(self, input_shape, output_shape, policy_input_var, Qvalue_input_var):
+        '''
+        :param num_input: the state shape
+        :param num_output: the output shape
+        :return: a determine policy network, a value network
+        '''
+        raise Exception('You should implement the _get_network function in the subclass')
+
+    def _predict(self, observation):
+        self.old_state = observation
+        return self.policy_predict_function(np.array([observation]))[0] + self.random_process.sample()
+
+    def _unpack_batch(self, batch):
+        train_X = []
+        action_list = []
+        now_features_list = []
+        done_list = []
+        reward_list = []
+        for old_features, action, now_features, reward, done in batch:
+            action_list.append(action)
+            now_features_list.append(now_features)
+            done_list.append(done)
+            reward_list.append(reward)
+            train_X.append(old_features)
+        train_X = np.array(train_X)
+        action_list = np.array(action_list)
+        now_features_list = np.array(now_features_list)
+        done_list = np.array(done_list)
+        reward_list = np.array(reward_list)
+        return train_X, action_list, now_features_list, done_list, reward_list
+
+    def update(self, observation, reward, done):
+        self.memory.add_sample((self.old_state, self.old_action, observation, reward, done))
+        batch = self.memory.sample(self.batch_size)
+        if batch is not None:
+            features, actions, next_features, dones, rewards = self._unpack_batch(batch)
+            target_next_action = self.target_policy_predict_function(next_features)
+            target = self.gamma * self.target_Qvalue_predict_function(next_features, target_next_action)+\
+                     (not done)*rewards
+            Qvalue_loss = lasagne.objectives.squared_error(target, self.Qvalue_predict_function(features, actions))
+            Qvalue_loss = Qvalue_loss.mean()
+            lasagne.updates.adam(Qvalue_loss, lasagne.layers.get_all_layers(self.Qvalue_network),)
+
+
---
+++ rl_tool/random_process.py
@@ -0,0 +1,19 @@
+import numpy as np
+from collections import deque
+
+
+class RandomProcess(object):
+    def sample(self):
+        pass
+
+
+class NormalMovingAverageProcess(RandomProcess):
+    def __init__(self, mu, sigma, M):
+        self.sample_ = lambda x: np.random.multivariate_normal(mu, sigma/M, size=x)
+        self.prev_M_samples = deque(self.sample_(M))
+
+    def sample(self):
+        r = np.mean(self.prev_M_samples, axis=0)
+        self.prev_M_samples.popleft()
+        self.prev_M_samples.append(self.sample_(1))
+        return r
